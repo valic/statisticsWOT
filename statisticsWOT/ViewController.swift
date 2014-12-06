d7ec0d82e86eb4bdb71f2156c6604333ce9b343b
Мялин Валентин <valic.my@gmail.com>
2014-12-06 22:37:05 +0200

    Initial Commit

---
+++ statisticsWOT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  statisticsWOT
+//
+//  Created by Мялин Валентин on 12/6/14.
+//  Copyright (c) 2014 Мялин Валентин. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
