f8a0929c07fcdafb8aa816945dc8fb2d55230de5
Мялин Валентин <valic.my@gmail.com>
2014-12-19 01:41:13 +0200

    Переписал функции, начал писать проверки

--- statisticsWOT/ViewController.swift
+++ statisticsWOT/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: NSViewController {
     @IBOutlet weak var outNickName: NSTextField!
     @IBAction func getStart(sender: AnyObject) {
     
-     var nickNameIn = inputNickName.stringValue
+        let nickNameIn = inputNickName.stringValue
+      
     
         println(nickNameIn)
         
@@ -27,40 +28,35 @@ class ViewController: NSViewController {
             
             return boardsDictionary
         }
-        func parseError(parse: NSDictionary) -> (codeError: Int?, message: String?, value:String?, Field:String?) {
+        func stringErorr(codeError: Int) -> () {
+          var stringErorr:String?
+            switch (codeError) {
+            case 402: stringErorr = "Не задано значение параметра search"
+            case 407: stringErorr = "Недостаточная длина параметра search. Минимум: 3 символа."
+            default: stringErorr = "Сервер вернул неизвестную ошибку"
+            }
+            println(stringErorr)
             
-            var codeError: Int?
-            var message:String?
-            var value:String?
-            var field:String?
-            var parseError:NSDictionary
-            
-            parseError = parse["error"] as NSDictionary!
-            
-            codeError = parseError["code"] as? Int
-            message = parseError["message"] as? String
-            value = (parseError["value"] as String)
-            field = (parseError["field"] as String)
-            
-            
-          return(codeError, message, value, field)
         }
-        
-        
-        //  Поиск по никнейму игрока и узнать его account_id.
-        func parseAccountID(userName: String) -> (account_id: NSNumber?, nickname: String?, status: String?, parseAccountIDOut:NSDictionary? )! {
+      
+        //  По никнейму игрока и узнаем его account_id.
+        func parseAccountID(userName: String) -> (account_id: NSNumber?, nickname: String?, status: String?, count: Int?,  codeError: Int?, message: String?, value: String?, Field: String?)! {
             
             var account_id:NSNumber?
             var nickname:String?
             var status:String?
-            var parseAccountIDOut:NSDictionary?
+            var count:Int?
+            var parseError:NSDictionary!
+            var codeError: Int?, message: String?, value: String?, field: String?
             
             let parseAccountID = (parseJSON(getJSON("https://api.worldoftanks.ru/wot/account/list/?application_id=c2f02a1fea169312db457a323fd41441&fields=account_id,nickname&type=exact&search=" + userName)))
             
             
-            if var parse = parseAccountID as? [String: AnyObject] {
-                
+            if let parse = parseAccountID as? [String: AnyObject] {
+                println(parse)
                 status = parse["status"] as String! // ‘ok’ — запрос выполнен успешно; ‘error’ — ошибка при выполнении запроса.
+                count = parse["count"] as Int!
+                
                 
                 if status == "ok" { // Проверка запрос выполнен успешно или нет.
                     
@@ -73,15 +69,17 @@ class ViewController: NSViewController {
                 }
                 else
                 {
-                  parseAccountIDOut = parseAccountID
+                    parseError = parseAccountID["error"] as NSDictionary
+                    
+                    codeError = parseError["code"] as? Int
+                    message = parseError["message"] as? String
+                    value = (parseError["value"] as String)
+                    field = (parseError["field"] as String)
                 }
             }
-            return (account_id,nickname, status!, parseAccountIDOut)
+            return (account_id, nickname, status, count, codeError, message, value, field)
         }
-        
-        
-
-        func parseAccountInfo(account_id: String) -> (battle_avg_xp: Int?, battles: Int?, status: String?, codeError: Int?, message: String?, value:String?, Field:String? )! {
+        func parseAccountInfo(account_id: NSNumber) -> (battle_avg_xp: Int?, battles: Int?, status: String?, codeError: Int?, message: String?, value:String?, Field:String? )! {
             
             var battle_avg_xp:Int?
             var battles: Int?
@@ -92,12 +90,12 @@ class ViewController: NSViewController {
             var field:String?
             var parseError:NSDictionary!
             
-            let parseAccountID = (parseJSON(getJSON("http://api.worldoftanks.ru/wot/account/info/?application_id=c2f02a1fea169312db457a323fd41441&fields=statistics.all.battles,statistics.all.battle_avg_xp&access_token=736bce94326dc5d2c7030928993d8c651c5eeb31&account_id=" + account_id)))
+            let parseAccountID = (parseJSON(getJSON("http://api.worldoftanks.ru/wot/account/info/?application_id=c2f02a1fea169312db457a323fd41441&fields=statistics.all.battles,statistics.all.battle_avg_xp&access_token=736bce94326dc5d2c7030928993d8c651c5eeb31&account_id=" + account_id.stringValue)))
             
             
             if let parse = parseAccountID as? [String: AnyObject] {
                 
-                println(parse)
+                //println(parse)
                 
                 status = parse["status"] as String! // ‘ok’ — запрос выполнен успешно; ‘error’ — ошибка при выполнении запроса.
                 
@@ -105,7 +103,7 @@ class ViewController: NSViewController {
                     
                     if let data = parse["data"] as? NSDictionary {
                         
-                        var ID = data[account_id] as? NSDictionary
+                        var ID = data[account_id.stringValue] as? NSDictionary
                         var statistics = ID!["statistics"] as? NSDictionary
                         var all = statistics!["all"] as? NSDictionary
                         battle_avg_xp = all!["battle_avg_xp"] as? Int
@@ -128,15 +126,26 @@ class ViewController: NSViewController {
             return (battle_avg_xp,battles, status!, codeError, message, value, field)
         }
         
-        let accountID = (parseAccountID(nickNameIn).account_id)?.stringValue // Идентификатор аккаунта игрока
+        let accountID = parseAccountID(nickNameIn)
+        var account_id:NSNumber?
         
-        if  accountID != nil { println(parseAccountInfo(accountID!)) }
-        
+        if accountID.status == "ok"  { // Проверка статуса
+            if accountID.count > 0 { 
+        account_id = accountID.account_id // Идентификатор аккаунта игрока
+                
+            let accountInfo = parseAccountInfo(account_id!)
+            println(accountInfo)
+            
+                
+                
+            }
+            else { outNickName.stringValue = "Ни найден пользователь с таким ником"}
+        }
         else
         {
-            parseError(parseAccountID(nickNameIn).parseAccountIDOut!)
+            stringErorr(accountID.codeError!)
         }
-        
+
     }
    
     
